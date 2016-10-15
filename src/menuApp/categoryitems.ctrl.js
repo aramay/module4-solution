126a2fd92edd758aabccb148102fb4cefed8d78d
“kasper341” <kasper_341@hotmail.com>
2016-10-14 19:19:57 -0700

    add category-items ctrl

---
+++ src/menuApp/categoryitems.ctrl.js
@@ -0,0 +1,18 @@
+(function () {
+
+    'use strict';
+
+    angular.module('MenuApp')
+    .controller('CategoryItemsController', CategoryItemsController);
+
+    CategoryItemsController.$inject = ['items', '$stateParams'];
+
+    function CategoryItemsController(items, $stateParams) {
+
+        var catectrl = this;
+
+        
+
+    }
+
+}) ();

16dcaabfded9eb4ff0682cff3adefeb9efb3c7bc
“kasper341” <kasper_341@hotmail.com>
2016-10-15 14:49:22 -0700

    cleaned comments

--- src/menuApp/category.ctrl.js
+++ src/menuApp/category.ctrl.js
@@ -4,16 +4,12 @@
 angular.module('MenuApp')
 .controller('CategoryController', CategoryController);
 
-// CategoryController.$inject = ['MenuDataService'];
 CategoryController.$inject = ['items'];
-// function CategoryController(MenuDataService) {
+
 function CategoryController(items) {
   var mainList = this;
 
   mainList.items = items;
-
-  // var categoryShortName = $stateParams.categoryShortName;
-
 
 }
 

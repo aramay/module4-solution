4427af9bc5e3b2b81675f37461139de7f50fe7ef
“kasper341” <kasper_341@hotmail.com>
2016-10-14 18:56:15 -0700

    call 'getItemsForCategory' to get cateory-items

--- src/menuApp/category.ctrl.js
+++ src/menuApp/category.ctrl.js
@@ -4,22 +4,39 @@
 angular.module('MenuApp')
 .controller('CategoryController', CategoryController);
 
-CategoryController.$inject = ['MenuDataService'];
+// CategoryController.$inject = ['MenuDataService'];
+CategoryController.$inject = ['$stateParams', 'items', 'MenuDataService'];
 // function CategoryController(MenuDataService) {
-function CategoryController(MenuDataService) {
+function CategoryController($stateParams, items, MenuDataService) {
   var mainList = this;
 
-  mainList.items = [];
+  mainList.items = items;
 
-  mainList.$onInit = function () {
-    MenuDataService.getItems()
-    .then(function (result) {
-      mainList.items = result;
+  var categoryShortName = $stateParams.categoryShortName;
 
-      console.log("main list controller ", mainList.items);
+  mainList.itemslist = [];
 
-    });
-  };
+  MenuDataService.getItemsForCategory(categoryShortName).
+  then(function (result) {
+      mainList.itemslist = result.menu_items;
+
+      console.log("inside CategoryController ", mainList.itemslist);
+  });
+
+
+  console.log("main list controller ", mainList.items);
+
+  console.log("outside CategoryController ", mainList.itemslist);
+
+  // mainList.$onInit = function () {
+  //   MenuDataService.getItems()
+  //   .then(function (result) {
+  //     mainList.items = result;
+  //
+  //     console.log("main list controller ", mainList.items);
+  //
+  //   });
+  // };
 }
 
 })();
